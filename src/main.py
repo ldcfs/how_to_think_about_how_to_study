32fe838088853f837cde2c2b18caccd6ce000c6c
Mateusz Steliga <mateusz.steliga@gmail.com>
2025-12-19 13:42:53 +0100

    mysterious equation

---
+++ src/main.py
@@ -0,0 +1,16 @@
+'''
+Exploring what it means to study
+
+Q: Why do we study?
+A: To learn abilities to become more valuable by gaining potential to serve others.
+
+Q: What changes after successful learning experience?
+A: We gain knowledge, abilities and wisdom (ability to apply knowledge).
+'''
+
+# def study(subject, instructions):
+
+'''
+P = W / t
+'''
+def 
